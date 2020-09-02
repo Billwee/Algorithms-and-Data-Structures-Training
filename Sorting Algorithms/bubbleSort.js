06b7e0381f74689b1a8272e68511c050bb18f8a4
Billwee <57924359+Billwee@users.noreply.github.com>
2020-09-02 13:21:55 -0400

    Added BS optimized

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -2,6 +2,8 @@
 
 // BubbleSort is not a very popular or fast sorting algorithm
 
+// BubbleSort has a time complexity of O(n^2)
+
 // BS will use two pointers srating at the first and second indexes of the array and compare them.
 // If the first index value is greater than the second, their values are swapped. If not then
 // nothing happens. After the comparison, the two indexes move up and element and compare again.
@@ -52,3 +54,33 @@ console.log(bubbleSort2([1, 4, 9, 16, 33, 74]));
 const swap = (arr, idx1, idx2) => {
   [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
 };
+
+
+// These BubbleSorts, while working, aren't exactly as efficent as they can be.
+// for example.. if the last half of the list is already sorted. It will still 
+// iterate through them while only removing one at the end at a time.
+// If we add a boolean that changes when no swaps are done we can effectivly 
+// end the sorting a lot earlier.
+
+function bubbleSort3(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      let temp = arr[j];
+      if (arr[j] > arr[j + 1]) {
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) return arr;
+  }
+  return arr;
+}
+
+console.log(bubbleSort3([111, 1214, 39, 146, 323, 674]));
+
+
+
+
