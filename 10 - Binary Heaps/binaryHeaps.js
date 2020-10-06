b89802a639cb22f49a4f3984ce70644f826e889e
Bill Kamberos <bill.kamberos@gmail.com>
2020-10-06 17:13:50 -0400

    Finished extractMax on BHs

--- 10 - Binary Heaps/binaryHeaps.js
+++ 10 - Binary Heaps/binaryHeaps.js
@@ -45,8 +45,9 @@ class MaxBinaryHeap {
     constructor(values) {
         this.values = values;
     }
-
-    // Insert method - Adds vlaues to the heap
+    // --
+    // --Insert method-- - Adds vlaues to the heap
+    // --
     // This involves pushing a value into the array and then bubbling it up to the
     // correct position. This is done by comparing the inserted value to it's
     // parents value. If it's more then it's parent. They're swapped. This is
@@ -58,8 +59,6 @@ class MaxBinaryHeap {
 
         let index = this.values.length - 1;
         let parentIndex = Math.max(0, Math.floor((index - 1) / 2));
-
-        console.log(index, parentIndex);
 
         while (true) {
             if (this.values[index] > this.values[parentIndex]) {
@@ -94,7 +93,18 @@ class MaxBinaryHeap {
     //     }
     // }
 
-    // Removing method - Removing a number from a heap
+    // let heap = new MaxBinaryHeap([]);
+    // console.log(heap.insert(90));
+    // console.log(heap.insert(12));
+    // console.log(heap.insert(33));
+    // console.log(heap.insert(45));
+    // console.log(heap.insert(84));
+    // console.log(heap.insert(100));
+    // console.log(heap.insert(1));
+
+    // --
+    // --Removing method-- - Removing a number from a heap
+    // --
     // This is mostly used for removing the root from a heap. The procedure
     // for deleting the root from the heap (effectivey extracting the max/min
     // value) and restoring the properties into a new heap is called down-heap.
@@ -104,20 +114,127 @@ class MaxBinaryHeap {
     // - swap the first value in the values property with the last
     // - pop from the values property
     // - have the new root down-heap to the correct spot
-    //
+    //    + parent index starts at 0
+    //    + find the indexes of it's children 2*index + 1 and 2*index + 2 (check if in bounds)
+    //    + if either value is larger swap them, if both are swap with larger value
+    //    + child index you swapped becomes the parent index now
+    //    + keep looping until neither child is larger
+    //    + return the old root
 
     extractMax() {
+        if (this.values.length === 1) {
+            let oldRoot = this.values[0];
+            this.values.pop();
+            return oldRoot;
+        }
+        let oldRoot = this.values[0];
         // prettier-ignore
-        this.values[0], this.values[this.values.length - 1] = this.values[this.values.length - 1], this.values[0];
+        [this.values[0], this.values[this.values.length - 1]] = [this.values[this.values.length - 1], this.values[0]];
         this.values.pop();
+
+        let idx = 0;
+
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChildValue = null;
+            let rightChildValue = null;
+
+            if (leftChildIdx < this.values.length) {
+                leftChildValue = this.values[leftChildIdx];
+            } else {
+                leftChildValue = -Infinity;
+            }
+            if (rightChildIdx < this.values.length) {
+                rightChildValue = this.values[rightChildIdx];
+            } else {
+                rightChildValue = -Infinity;
+            }
+
+            // prettier-ignore
+            if(leftChildValue > this.values[idx] || rightChildValue > this.values[idx]){
+                let larger = Math.max(leftChildValue, rightChildValue);
+
+                if(larger === leftChildValue){
+                    // prettier-ignore
+                    [this.values[idx], this.values[leftChildIdx]] = [this.values[leftChildIdx], this.values[idx]];
+                    idx = leftChildIdx;
+                }else{
+                    // prettier-ignore
+                    [this.values[idx], this.values[rightChildIdx]] = [this.values[rightChildIdx], this.values[idx]];
+                    idx = rightChildIdx;
+                }
+            }else{
+                break;
+            }
+        }
+
+        // return this.values; - // For testing
+        return oldRoot;
     }
+
+    // -- INSTRUCTOR SOLUTION
+    // extractMax() {
+    //     const max = this.values[0];
+    //     const end = this.values.pop();
+    // if(this.values.length > 0){  // Solves 1 index array edgecase
+    //     this.values[0] = end;
+    //     this.sinkdown();
+    // }
+    //     return this.values; - // for testing
+    //     return max;
+    // }
+
+    // sinkdown() {
+    //     let idx = 0;
+    //     const length = this.values.length;
+    //     const element = this.values[0];
+    //     while (true) {
+    //         let leftChildIdx = 2 * idx + 1;
+    //         let rightChildIdx = 2 * idx + 2;
+    //         let leftChild, rightChild;
+    //         let swap = null;
+
+    //         if (leftChildIdx < length) {
+    //             leftChild = this.values[leftChildIdx];
+    //             if (leftChild > element) {
+    //                 swap = leftChildIdx;
+    //             }
+    //         }
+    //         if (rightChildIdx < length) {
+    //             rightChild = this.values[rightChildIdx];
+    //             if (
+    //                 (swap === null && rightChild > element) ||
+    //                 (swap !== null && rightChild > leftChild)
+    //             ) {
+    //                 swap = rightChildIdx;
+    //             }
+    //         }
+    //         if (swap === null) break;
+
+    //         this.values[idx] = this.values[swap];
+    //         this.values[swap] = element;
+    //         idx = swap;
+    //     }
+    // }
 }
 
 let heap = new MaxBinaryHeap([]);
-console.log(heap.insert(90));
-console.log(heap.insert(12));
-console.log(heap.insert(33));
-console.log(heap.insert(45));
-console.log(heap.insert(84));
-console.log(heap.insert(100));
-console.log(heap.insert(1));
+heap.insert(90);
+heap.insert(12);
+heap.insert(33);
+heap.insert(45);
+heap.insert(84);
+heap.insert(100);
+heap.insert(1);
+console.log(heap.extractMax());
+
+let heap2 = new MaxBinaryHeap([]);
+heap2.insert(41);
+heap2.insert(39);
+heap2.insert(33);
+heap2.insert(18);
+heap2.insert(27);
+heap2.insert(12);
+heap2.insert(55);
+console.log(heap2.extractMax());
